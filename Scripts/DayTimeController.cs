6756435eb5563b435a3800c65d8b4b8aa850b9e4
yassershahofficial <geniususer4u@gmail.com>
2025-02-24 14:18:15 +0800

    Day Night Cycle

--- Scripts/DayTimeController.cs
+++ Scripts/DayTimeController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class DayTimeController : MonoBehaviour
@@ -10,22 +11,46 @@ public class DayTimeController : MonoBehaviour
     [SerializeField] Color DayLight = Color.white;
     [SerializeField] Color NightLight;
     [SerializeField] AnimationCurve NightTimeCurve;
+    [SerializeField] Light2D globalLight;
     [SerializeField] TextMeshProUGUI TimeText;
+    [SerializeField] float TimeScale = 120;
 
     float secondInDay = 86400f;
     float time = 0;
+    int days = 0;
 
     private void Update()
     {
-        time += Time.deltaTime*120; 
-        time %= secondInDay; // Reset time after a full day
+        time += Time.deltaTime*TimeScale; 
+        if(time >= secondInDay){
+            TimeByDays();
+        }
+        
+        TimeUI(time);
+        LightCurve(time);
 
+    }
+
+    private void TimeByDays()
+    {
+        time %= secondInDay; // Reset time after a full day
+        days += 1;
+    }
+
+    private void TimeUI(float time){
         int seconds = Mathf.FloorToInt(time % 60);
         int minutes = Mathf.FloorToInt((time % 3600) / 60); 
         int hours = Mathf.FloorToInt(time / 3600); 
         
-        TimeText.text = $"{hours:00}:{minutes:00}:{seconds:00}"; 
+        TimeText.text = hours.ToString("00:")+minutes.ToString("00:")+seconds.ToString("00"); 
+    }
 
+    private void LightCurve(float time){
+        float hoursOnly = time / 3600f;
+        float curve = NightTimeCurve.Evaluate(hoursOnly);
+        Color light = Color.Lerp(DayLight, NightLight, curve);
+
+        globalLight.color = light;
     }
     
 }
